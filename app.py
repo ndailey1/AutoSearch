850f5a49b8646a2cb0f358403ab74a07a057eee5
Nathan Dailey <98418830+ndailey1@users.noreply.github.com>
2024-01-17 18:19:54 -0500

    Update app.py

--- app.py
+++ app.py
@@ -19,8 +19,8 @@ with open("NewBritainData.csv", "r") as csv_file:
 
         time.sleep(1)
 
-    #INPUTTING LOGIN INFO - using "Sign-In As Guest" option for testing.
-    
+#INPUTTING LOGIN INFO - using "Sign-In As Guest" rather than signing in just for testing.
+
         #username = driver.find_element("xpath", '/html/body/form/div[4]/div[2]/div/div[3]/table/tbody/tr/td[1]/span[1]/div/div[2]/table/tbody/tr[1]/td[2]/input')
         #username.send_keys('chriscooper')
 
@@ -29,19 +29,23 @@ with open("NewBritainData.csv", "r") as csv_file:
 
         #time.sleep(1)
 
-    #CLICKING "LOGIN"
-        #**change sign in xpath when not signing in as guest.
+#CLICKING "LOGIN"
+    #**change xpath when not signing in as guest.
 
         sign_in_button = driver.find_element("xpath", '/html/body/form/div[4]/div[3]/div/div[3]/table/tbody/tr/td[1]/span[1]/div/div[2]/table/tbody/tr[3]/td/input[2]')
         sign_in_button.click()
 
         #time.sleep(1)
 
-        exit()
+#Needs to go to New Britain page, then search by vol/page
 
 
 #SEARCHER PAGE
 
-        #volume = driver.find_element("xpath", '//*[@id="ctl00_cphMain_txtBookNumber"]')
-        #name.sendkeys(line[4])
+        #volume = driver.find_element("xpath", '/html/body/form/div[4]/div[2]/div/div[3]/div/center/div/table/tbody/tr[1]/td[3]/input')
+        #volume.sendkeys(line[5])
+
+        #page = driver.find_element("xpath", '/html/body/form/div[4]/div[2]/div/div[3]/div/center/div/table/tbody/tr[1]/td[5]/input')
+        #page.sendkeys(line[6])
+
 
