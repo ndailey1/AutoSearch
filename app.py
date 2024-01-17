efe1fdd17ad483c877ccc2c81d66b44678e75ec5
Nathan Dailey <98418830+ndailey1@users.noreply.github.com>
2024-01-17 18:13:17 -0500

    Update app.py

--- app.py
+++ app.py
@@ -12,27 +12,28 @@ with open("NewBritainData.csv", "r") as csv_file:
 # WEB AUTOMATION
 
 #LOGIN PAGE
+
     for line in csv_reader:
-        #print(line)
         driver = webdriver.Chrome()
         driver.get('https://connecticut-townclerks-records.com/User/Login.aspx?bSesExp=True')
 
         time.sleep(1)
 
-    #INPUTTING LOGIN INFO
-        username = driver.find_element("xpath", '/html/body/form/div[4]/div[2]/div/div[3]/table/tbody/tr/td[1]/span[1]/div/div[2]/table/tbody/tr[1]/td[2]/input')
-        username.send_keys('chriscooper')
+    #INPUTTING LOGIN INFO - using "Sign-In As Guest" option for testing.
+    
+        #username = driver.find_element("xpath", '/html/body/form/div[4]/div[2]/div/div[3]/table/tbody/tr/td[1]/span[1]/div/div[2]/table/tbody/tr[1]/td[2]/input')
+        #username.send_keys('chriscooper')
 
-        password = driver.find_element("xpath", '/html/body/form/div[4]/div[2]/div/div[3]/table/tbody/tr/td[1]/span[1]/div/div[2]/table/tbody/tr[2]/td[2]/input')
-        password.send_keys('101Corporate')
+        #password = driver.find_element("xpath", '/html/body/form/div[4]/div[2]/div/div[3]/table/tbody/tr/td[1]/span[1]/div/div[2]/table/tbody/tr[2]/td[2]/input')
+        #password.send_keys('101Corporate')
 
-        time.sleep(1)
+        #time.sleep(1)
 
     #CLICKING "LOGIN"
-        #**change sign in xpath when above lines are active
+        #**change sign in xpath when not signing in as guest.
 
-        #sign_in_button = driver.find_element("xpath", '/html/body/form/div[4]/div[3]/div/div[3]/table/tbody/tr/td[1]/span[1]/div/div[2]/table/tbody/tr[3]/td/input[2]')
-        #sign_in_button.click()
+        sign_in_button = driver.find_element("xpath", '/html/body/form/div[4]/div[3]/div/div[3]/table/tbody/tr/td[1]/span[1]/div/div[2]/table/tbody/tr[3]/td/input[2]')
+        sign_in_button.click()
 
         #time.sleep(1)
 
@@ -44,6 +45,3 @@ with open("NewBritainData.csv", "r") as csv_file:
         #volume = driver.find_element("xpath", '//*[@id="ctl00_cphMain_txtBookNumber"]')
         #name.sendkeys(line[4])
 
-
-
-#LEFT - webdriver keeps shutting down before executing login click. How to add "if" and "then" commands? New script after the login is executed? New script for each page?
